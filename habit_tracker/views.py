eae7aa97660b4d38d5f255f7af080157391c6d28
medina325 <medina_cdz@hotmail.com>
2023-01-21 20:38:49 -0400

    Primeiro esboço da landing page do site - organizando html e css

--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
+    """
+    View that renders the main page
+    """
+
     return render(request, 'habit_tracker/index.html')
